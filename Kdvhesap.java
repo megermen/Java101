6b11ebfca1594f28427685b956a07117de4bbbc3
Mert Enver Germen <101748686+megermen@users.noreply.github.com>
2022-09-02 22:56:21 +0300

    Update Kdvhesap.java

--- Kdvhesap.java
+++ Kdvhesap.java
@@ -2,31 +2,22 @@ import java.util.Scanner;
 public class Abc {
     public static void main(String[] args) {
     
-        double tutar , kdvoran = 0.18, kdvtutar , kdvlifiyat;
+        double tutar , kdvoran , kdvtutar , kdvlifiyat;
 
         Scanner until = new Scanner(System.in);
         System.out.println("bir tutar giriniz :");
         tutar = until.nextDouble();
         
-        if (tutar <= 1000){
-            kdvoran = 0.18;
-            kdvtutar = tutar * kdvoran;
-            kdvlifiyat = tutar + kdvtutar;
+        kdvoran = (tutar <= 1000)? 0.18 : 0.08;
+        kdvtutar = tutar * kdvoran;
+        kdvlifiyat = tutar + kdvtutar;
             
-            System.out.println("KDV'siz tutar = "+tutar);
-            System.out.println("KDV oranı = "+kdvoran);
-            System.out.println("KDV tutarı = "+kdvtutar);
-            System.out.println("KDV'li fiyat = "+kdvlifiyat);
-        }
-        else {
-            kdvoran = 0.08;
-            kdvtutar = tutar * kdvoran;
-            kdvlifiyat = tutar + kdvtutar;
+           
             
-            System.out.println("KDV'siz tutar = "+tutar);
-            System.out.println("KDV oranı = "+kdvoran);
-            System.out.println("KDV tutarı = "+kdvtutar);
-            System.out.println("KDV'li fiyat = "+kdvlifiyat);
+        System.out.println("KDV'siz tutar = "+tutar);
+        System.out.println("KDV oranı = "+kdvoran);
+        System.out.println("KDV tutarı = "+kdvtutar);
+        System.out.println("KDV'li fiyat = "+kdvlifiyat);
 
         }
     }
