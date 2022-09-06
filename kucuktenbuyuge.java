197198c0ec4d32b5803af9f846cc866ebb88d830
Mert Enver Germen <101748686+megermen@users.noreply.github.com>
2022-09-06 23:54:46 +0300

    Update kucuktenbuyuge.java

--- kucuktenbuyuge.java
+++ kucuktenbuyuge.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class deneme {
+public class sıralama {
     public static void main(String[] args) {
 
 
@@ -19,20 +19,20 @@ public class deneme {
 
         if ((a < b && a < c)) {
             if (b < c) {
-                System.out.println("a<b<c");
+                System.out.println("a < b < c");
             } else {
                 System.out.println("a < c < b");
             }
         } else if ((b < c && b < a)) {
             if (a < c) {
-                System.out.println("b<c<a");
+                System.out.println("b < c <a");
             } else {
-                System.out.println("b<a<c");
+                System.out.println("b< a < c");
 
                 {
                     if ((c < a && c < b)) {
                         if (a < b) {
-                            System.out.println("c<a<b");
+                            System.out.println("c < a < b");
                         } else {
                             System.out.println("c<b<a");
                         }
